a03a9c9b4042539064586512771d5f59314b6eec
Mustafa Ibrahim <mustafa7ibrahim@gmail.com>
2023-09-10 23:19:30 +0300

    final v

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -1,20 +1,9 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const slugify = require('slugify');
 
-const replaceTemplate = (temp, product) => {
-    let output = temp.replace(/{%PRODUCT_NAME%}/g, product.productName);
-    output = output.replace(/{%IMAGE%}/g, product.image);
-    output = output.replace(/{%PRICE%}/g, product.price);
-    output = output.replace(/{%FROM%}/g, product.from);
-    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
-    output = output.replace(/{%QUANTITY%}/g, product.quantity);
-    output = output.replace(/{%DESCRIPTION%}/g, product.description);
-    output = output.replace(/{%ID%}/g, product.id);
-
-    if (!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
-    return output;
-}
+const replaceTemplate = require('./modules/replace-template');
 
 const cartTemplate = fs.readFileSync(`${__dirname}/templates/template-cart.html`, 'utf-8');
 const overviewTemplate = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
@@ -22,6 +11,9 @@ const productTemplate = fs.readFileSync(`${__dirname}/templates/template-product
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
+
+const slugs = dataObj.map(el => slugify(el.productName, { lower: true }));
+console.log(slugs);
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
 
